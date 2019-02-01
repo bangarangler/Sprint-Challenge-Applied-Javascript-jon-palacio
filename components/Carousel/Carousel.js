6dbc531089ffed6f241a925cbdd5c989a8479140
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-01 13:36:10 -0500

    Commented code better for future use

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,3 +1,4 @@
+// class constructor here
 class Carousel {
   constructor(sel) {
     // set this binding
@@ -11,26 +12,39 @@ class Carousel {
     this.index = 0;
     this.imgs[this.index].style.display = "block";
 
+    // eventlistener for left and right buttons
     this.leftBtn.addEventListener("click", () => this.goLeft());
     this.rightBtn.addEventListener("click", () => this.goRight());
   }
+  // methods to scroll through the nodelist of the images
   goLeft() {
+    // sets the display back to none before showing the next img
     this.imgs[this.index].style.display = "none";
+    // if img index is 0 then index is end img of nodelist otherwise got to
+    // next lowest index
     this.index === 0 ? (this.index = this.imgs.length - 1) : this.index--; //jshint ignore: line
 
+    // display selected image
     this.imgs[this.index].style.display = "block";
   }
 
   goRight() {
+    // sets display back to none before showing next img
     this.imgs[this.index].style.display = "none";
+    // check img index, if it's the last in the nodelist sets it to 0
+    // otherwise increments by one.
     this.index === this.imgs.length - 1 ? (this.index = 0) : this.index++; //jshint ignore: line
+    // displays img at current index
     this.imgs[this.index].style.display = "block";
   }
 }
 
+// select imgs and store them in variable
 let carousels = document.querySelectorAll(".carousel img");
-console.log(carousels);
 
+//console.log(carousels);
+
+//loop through them and create a new img see top of file for class
 carousels.forEach(sel => new Carousel(sel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
